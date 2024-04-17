b2a16d242b03141d6f1aff2cf0b93dd5df38ac90
holdeno0 <hbudiman1@my.bcit.ca>
2024-04-16 19:14:48 -0700

    adding content

--- components/Question/Question.js
+++ components/Question/Question.js
@@ -11,17 +11,16 @@ export default function Question({ question, onAnswerClick }) {
         <div className={styles.answers}>
           {question.answerOptions.map((answer, index) => (
             <button key={index} onClick={() => onAnswerClick(answer.score)} className={styles.button}>
-              <div className={styles.answerContentContainer}>
+              {/* <div className={styles.answerContentContainer}> */}
                 <div className={styles.answerContent}>
                 {answer.image}
                 <p>{answer.answerText}</p>
                 </div>
-              </div>
+              {/* </div> */}
             </button>
           ))}
         </div>
         <footer className={styles.footer}>
-        
         </footer>
         </div>
       </div>
