45eeb78aa370bbacee95d417f469463a1b146411
holdeno0 <hbudiman1@my.bcit.ca>
2024-04-25 13:23:50 -0700

    adding takeQuizCardand changing readme

---
+++ components/TakeQuizCard/index.js
@@ -0,0 +1,21 @@
+import styles from './TakeQuizCard.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+
+
+
+export default function TakeQuizCard() {
+    return (
+        <div className={styles.takeQuizCardContainer}>
+            <div className={styles.takeQuizCardInnerContainer}>
+                <div className={styles.takeQuizContent}>
+                <h1>Out of Ideas</h1>
+                <p>Take our quiz and receive a personalized recommendation tailored to your preferences, interests, and needs!</p>
+                <Link href="/quiz">
+                <button>Take Quiz</button>
+                </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
