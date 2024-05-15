f19323649680d1080dc5f03fdab7a98dac7003db
jdaniera <jasminejasmine14@hotmail.com>
2024-05-14 21:50:19 -0700

    fixing saved page

--- components/ClassesCard/index.js
+++ components/ClassesCard/index.js
@@ -1,38 +1,38 @@
-import React from "react";
+import React from 'react';
 import Styles from './ClassesCard.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
-import SavedButton from "../SavedButton";
+import SavedButton from '../SavedButton';
 
-export default function ClassesCard({title, subtitle, image, alt, link, buttonText, query, prompt}) {
-    
-    return(
-        <div className={Styles.classesCardContainer}>
-            <div className={Styles.classesCardInnerContainer}>
-                <div className={Styles.imageContainer}>
-                    <Image src={image} alt={alt} width={191} height={168}/>
-                </div>
-                <div className={Styles.cardHeader}>
-                    <p>{title}</p>
-                    <SavedButton prompt={prompt}/>
-
-                    {/* <Image src="/images/classesFeaturedCard/heartIcon.svg" width={16.7} height={15.5} alt="Heart Icon" />                 */}
-                </div>
-                <div className={Styles.cardSubtitle}>
-                    <p>{subtitle}</p>
-                </div>
-                <div className={Styles.cardButton}>
-                    <Link
-                        href={{
-                            pathname: link,
-                            query: query || {}
-                        }}>
-                        <button>{buttonText}</button>
-                    </Link>
-                </div>
-            </div>
+export default function ClassesCard({ title, subtitle, image, alt, link, buttonText, query, id }) {
+  return (
+    <div className={Styles.classesCardContainer}>
+      <div className={Styles.classesCardInnerContainer}>
+        <div className={Styles.imageContainer}>
+          <Image src={image} alt={alt} width={191} height={168} />
         </div>
-        
-    ) 
-
+        <div className={Styles.cardHeader}>
+          <p>{title}</p>
+          <SavedButton 
+            title={title}
+            subtitle={subtitle}
+            image={image}
+            alt={alt}
+            link={link}
+            buttonText={buttonText}
+            query={query}
+            id={id}
+          />
+        </div>
+        <div className={Styles.cardSubtitle}>
+          <p>{subtitle}</p>
+        </div>
+        <div className={Styles.cardButton}>
+          <Link href={{ pathname: link, query: query || {} }}>
+            <button>{buttonText}</button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
